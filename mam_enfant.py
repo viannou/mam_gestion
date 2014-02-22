a4f29bbebf456fd0a4428e2c93a3dbbf9d35e8e1
viannou <viannou@gmail.com>
2014-02-22 22:35:19 +0100

    auto

--- mam_enfant.py
+++ mam_enfant.py
@@ -22,11 +22,11 @@ class mam_enfant(osv.Model):
         ),
         'date_naiss': fields.date('Date de naissance',required=True, help='Date de naissance de l''enfant'),
         'am_id': fields.many2one('mam.am','Assistante maternelle de référence',required=True, help='Assistante maternelle de référence pour l''enfant'),
-#liste des contacts
-#        'rel_contact_id': fields.one2many('mam.rel_enfant_contact', 'enfant_id', 'Liste des contacts', help='Liste des contacts de l''enfant'),
         'contact_ids': fields.many2many('mam.contact','mam_enfant_contact_rel','contact_id','enfant_id',string="Contacts"),
         'allergies': fields.text('Allergies', help='Allergies de l''enfant'),
         'recommandations': fields.text('Recommandations générales', help='Recommandations générales pour l''enfant'),
+        'presence_ids': fields.one2many('mam.presence', 'enfant_id', 'Liste des présences', help='Liste des présences de l''enfant'),
+
 #liste des contrats
     }
     _rec_name = 'nomprenom'
