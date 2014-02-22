90889c1f3f527b6bc088034c46fbefa3811f4a42
viannou <viannou@gmail.com>
2014-02-22 17:27:25 +0100

    auto

--- __openerp__.py
+++ __openerp__.py
@@ -1,14 +1,14 @@
 {
-	"name" : "Gestion de MAM",
-	"version" : "1.1",
-	"author" : "Vianney Leclercq",
-	"website" : "http://www.castelbambins.com/",
-	"category" : "Tools",
-	"description": """ Gestion de Maison d'Assistante Maternelle """,
-	"depends" : ['base'],
-	"init_xml" : [ ],
-	"demo_xml" : [ ],
-	"update_xml" : ['mam_gestion_view.xml'],
-	"installable": True
+    "name" : "Gestion de MAM",
+    "version" : "1.1",
+    "author" : "Vianney Leclercq",
+    "website" : "http://www.castelbambins.com/",
+    "category" : "Tools",
+    "description": """ Gestion de Maison d'Assistante Maternelle """,
+    "depends" : ['base'],
+    "init_xml" : [ ],
+    "demo_xml" : [ ],
+    "update_xml" : ['mam_gestion_view.xml'],
+    "installable": True
 }
 
--- mam_jour.py
+++ mam_jour.py
@@ -2,25 +2,25 @@
 from osv import fields,osv
 
 class mam_jour(osv.Model):
-	def _get_fullname(self, cr, uid, ids, name, args, context=None):
-		"""Test de field function"""
-		result = dict()
-		for id in ids:
-			result[id] = "ah " + id
-		return result
+    def _get_fullname(self, cr, uid, ids, name, args, context=None):
+        """Test de field function"""
+        result = dict()
+        for id in ids:
+            result[id] = "ah " + id
+        return result
 
-	_name = 'mam.jour'
-	_description = "Jour"
-	_columns = {
+    _name = 'mam.jour'
+    _description = "Jour"
+    _columns = {
         "fullname": fields.function(
             _get_fullname,
             type="char",
             string="Full Name",
             store=None,
             #select=True,
-		)
-	}
+        )
+    }
 
-	
+    
 mam_jour()
 
