b7a0188986b893280ea216f329a69f0fd35c77f5
viannou <viannou@gmail.com>
2014-02-22 18:54:24 +0100

    a

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-import mam_gestion, mam_jour
+import mam_gestion, mam_enfant
 
--- mam_gestion.py
+++ mam_gestion.py
@@ -33,37 +33,6 @@ class mam_am(osv.Model):
     _rec_name = 'nomprenom'
 mam_am()
 
-class mam_enfant(osv.Model):
-    _name = 'mam.enfant'
-    _description = "Enfant"
-    def _get_nomprenom(self, cr, uid, ids, name, args, context=None):
-        """nom affichable de l'enfant"""
-        result = {}
-        for record in self.browse(cr, uid, ids, context=context):
-            result[record.id]= record.prenom + " " + record.nom
-        return result
-    _columns = {
-        'nom': fields.char('Nom',size=50,required=True, help='Nom de l''enfant'),
-        'prenom': fields.char('Prénom',size=50,required=True, help='Prénom de l''enfant'),
-        'nomprenom': fields.function(
-            _get_nomprenom,
-            type="char",
-            string="Nom Complet",
-            store=None,
-            #select=True,
-        ),
-        'date_naiss': fields.date('Date de naissance',required=True, help='Date de naissance de l''enfant'),
-        'am_id': fields.many2one('mam.am','Assistante maternelle de référence',required=True, help='Assistante maternelle de référence pour l''enfant'),
-#liste des contacts
-#        'rel_contact_id': fields.one2many('mam.rel_enfant_contact', 'enfant_id', 'Liste des contacts', help='Liste des contacts de l''enfant'),
-        'contact_ids': fields.many2many('mam.contact','mam_enfant_contact_rel','contact_id','enfant_id',string="Contacts"),
-        'allergies': fields.text('Allergies', help='Allergies de l''enfant'),
-        'recommandations': fields.text('Recommandations générales', help='Recommandations générales pour l''enfant'),
-#liste des contrats
-    }
-    _rec_name = 'nomprenom'
-mam_enfant()
-
 class mam_contact(osv.Model):
     _name = 'mam.contact'
     _description = "Contact"
