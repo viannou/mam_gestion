adbf53efae7ed0ef959cdff1f35b89a44632d3e1
viannou <viannou@gmail.com>
2014-02-23 01:28:09 +0100

    auto

--- mam_enfant.py
+++ mam_enfant.py
@@ -23,6 +23,7 @@ class mam_enfant(osv.Model):
                 if date_debut.date() == date.today():
                     result[enfant.id]['today_presence_ids'].append(presence.id)
                     if date_debut < datetime.now():
+                        print presence.date_fin
                         if presence.date_fin is None:
                             result[enfant.id]['today_est_present'] = True
                         elif datetime.strptime(presence.date_fin,'%Y-%m-%d %H:%M:%S') > datetime.now():
