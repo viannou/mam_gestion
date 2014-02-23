cad32e035743c454a4259f940257e8831c03a8af
viannou <viannou@gmail.com>
2014-02-23 01:18:15 +0100

    auto

--- mam_enfant.py
+++ mam_enfant.py
@@ -22,8 +22,11 @@ class mam_enfant(osv.Model):
                 date_debut = datetime.strptime(presence.date_debut,'%Y-%m-%d %H:%M:%S')
                 if date_debut.date() == date.today():
                     result[enfant.id]['today_presence_ids'].append(presence.id)
-                    if date_debut < datetime.now() and ((presence.date_fin is None) or datetime.strptime(presence.date_fin,'%Y-%m-%d %H:%M:%S') > datetime.now()):
-                        result[enfant.id]['today_est_present'] = True
+                    if date_debut < datetime.now():
+                        if presence.date_fin is None:
+                            result[enfant.id]['today_est_present'] = True
+                        elif datetime.strptime(presence.date_fin,'%Y-%m-%d %H:%M:%S') > datetime.now()):
+                            result[enfant.id]['today_est_present'] = True
         return result
     _columns = {
         'nom': fields.char('Nom',size=50,required=True, help='Nom de l''enfant'),
