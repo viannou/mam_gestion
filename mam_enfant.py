7a777f1359273c5ee8b6c6f2bd7f23d5696f1fba
viannou <viannou@gmail.com>
2014-02-23 01:39:26 +0100

    auto

--- mam_enfant.py
+++ mam_enfant.py
@@ -20,6 +20,7 @@ class mam_enfant(osv.Model):
             result[enfant.id]['today_presence_ids'] = list()
             result[enfant.id]['today_est_present'] = False
             for presence in enfant.presence_ids:
+                pprint(presence)
                 date_debut = datetime.strptime(presence.date_debut,'%Y-%m-%d %H:%M:%S')
                 if date_debut.date() == date.today():
                     result[enfant.id]['today_presence_ids'].append(presence.id)
