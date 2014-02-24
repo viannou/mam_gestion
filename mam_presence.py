85183d2cbbab616040fb55ebec9625784de01bf2
viannou <viannou@gmail.com>
2014-02-24 21:30:57 +0100

    auto

--- mam_presence.py
+++ mam_presence.py
@@ -10,7 +10,7 @@ class mam_jour_e(osv.Model):
         'enfant_id': fields.many2one('mam.enfant','Enfant',required=True, help='Enfant concerné par la journée'),
         'mange_midi': fields.boolean('Mange le midi', help='Prise du repas du midi'),
         'mange_gouter': fields.boolean('Mange au gouter', help='Prise du gouter'),
-        'frais_montant': fields.float('Montant des frais', digits=(6,2) help='Montant des frais en euros'),
+        'frais_montant': fields.float('Montant des frais', digits=(6,2), help='Montant des frais en euros'),
         'frais_libelle': fields.char('Libellé des frais', help='Libellé des frais'),
         'commentaire': fields.text('Commentaire journée', help='Commentaire sur la présence ou l''absence'),
     }
