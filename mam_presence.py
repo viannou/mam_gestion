0f9969b789eb811aad6a957e96264244e3372a04
viannou <viannou@gmail.com>
2014-03-01 14:37:43 +0100

    auto

--- mam_presence.py
+++ mam_presence.py
@@ -84,26 +84,6 @@ class mam_jour_e(osv.Model):
             
         return True
 mam_jour_e()
-''''''''''''''
-    def action_creer_jours(self, cr, uid, ids, context=None):
-        """ajoute pour l'enfant sélectionné le jour type sélectionné pour les 90 jours à venir (sauf samedi dimanche)"""
-        for enfant in self.browse(cr, uid, ids, context=context):
-            print enfant.id, enfant.nomprenom, context
-            for date_d in (date.today() + timedelta(n) for n in range(90)):
-                print date_d
-                jour_type = self.pool.get('mam.jour_type')
-                jours_e_ids = jour_type.search(cr, uid, [('jour','=', date_d),('enfant_id','=',enfant.id)], context=context)
-                if not jours_e_ids: # le jour de l'enfant n'existe pas encore
-                    print "creation enfant ", enfant.nomprenom, " date ", date_d 
-                    jour_e.create(cr, uid,{ 'jour': date_d,'enfant_id' : enfant.id,})
-                    
-
-
-        # for jour_type in self.browse(cr, uid, ids, context=context):
-            # print jour_type.id, jour_type.libelle, jour_type.enfant_id.id, jour_type.enfant_id.nomprenom, context
-            # #jour_e_ids = jour_type.enfant_id.jour_e_ids
-        return True
-''''''''''''''
 
 # class mam_presence_e(osv.Model):
     # _name = 'mam.presence_e'
