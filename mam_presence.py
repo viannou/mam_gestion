51376171e58fe9c2ffcf77fac71feffbf382e760
viannou <viannou@gmail.com>
2014-03-11 21:34:08 +0100

    auto

--- mam_presence.py
+++ mam_presence.py
@@ -53,8 +53,8 @@ class mam_jour_e(osv.Model):
     _columns = {
         'jour': fields.date('Jour',required=True, help='La date'),
         'enfant_id': fields.many2one('mam.enfant','Enfant',required=True, help='Enfant concerné par la journée'),
-        'mange_midi': fields.boolean('Mange le midi', help='Prise du repas du midi'),
-        'mange_gouter': fields.boolean('Mange au gouter', help='Prise du gouter'),
+        'mange_midi': fields.boolean('Midi', help='Prise du repas du midi'),
+        'mange_gouter': fields.boolean('Gouter', help='Prise du gouter'),
         'frais_montant': fields.float('Montant des frais', digits=(6,2), help='Montant des frais en euros'),
         'frais_libelle': fields.char('Libellé des frais', help='Libellé des frais'),
         'commentaire': fields.text('Commentaire journée', help='Commentaire sur la présence ou l''absence'),
