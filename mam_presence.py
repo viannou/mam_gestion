a325cb9c3cb1b23579ec0097d4cc254a4ce0207a
viannou <viannou@gmail.com>
2014-03-19 20:07:12 +0100

    auto

--- mam_presence.py
+++ mam_presence.py
@@ -5,14 +5,14 @@ import re
 
 def verif_heures(hdebut, hfin, fin_obligatoire=False):
     try:
-        matchObj = re.match( r"^(\d+?)[- _.:;'hH]?(\d{1,2})[mM]?$", hdebut)
+        matchObj = re.match( r"^(\d+?)[- _.:;'hH]?(\d{1,2})[mM]?$",  hdebut)
         if matchObj:
             hdebut = "{:%H:%M}".format(datetime.strptime(matchObj.group(1)+":"+matchObj.group(2),"%H:%M"))
         else:
             return False
         if not fin_obligatoire and (hfin == False or hfin == ""):
             return [hdebut,""]
-        matchObj = re.match( r"^(\d+?)[- _.:;'hH]?(\d{1,2})[mM]?$", hfin)
+        matchObj = re.match( r"^(\d+?)[- _.:;'hH]?(\d{1,2})[mM]?$",  hfin)
         if matchObj:
             hfin = "{:%H:%M}".format(datetime.strptime(matchObj.group(1)+":"+matchObj.group(2),"%H:%M"))
         else:
