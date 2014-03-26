918150c80cc98f75ac8dda478d72b96a01d3a60b
viannou <viannou@gmail.com>
2014-03-26 23:00:17 +0100

    auto

--- mam_enfant.py
+++ mam_enfant.py
@@ -135,7 +135,7 @@ class mam_enfant(osv.Model):
             jour_e_ids = mam_jour_e.search(cr, uid, [('enfant_id','=',enfant.id)], context=context)
             liste = []
             for jour_e in jour_e_ids:
-                if not (enfant.id, jour_e.jour.year, jour_e.jour.month) in liste
+                if (not (enfant.id, jour_e.jour.year, jour_e.jour.month) in liste)
                     liste.append( (enfant.id, jour_e.jour.year, jour_e.jour.month) )
                     mois_e_ids = mam_mois_e.search(cr, uid, [('enfant_id','=',enfant.id),('annee','=', jour_e.jour.year),('mois','=', jour_e.jour.month)], context=context)
                     if not mois_e_ids: # le mois de l'enfant n'existe pas encore
