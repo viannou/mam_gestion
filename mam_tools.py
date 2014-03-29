dd4667036fdcc5b6fb3013c72c91861f7a9fbfdd
viannou <viannou@gmail.com>
2014-03-29 19:57:54 +0100

    fichier tools

---
+++ mam_tools.py
@@ -0,0 +1,27 @@
+# -*- coding: utf8 -*-
+from datetime import datetime,date,timedelta
+import re
+
+def verif_heures(hdebut, hfin, fin_obligatoire=False):
+    try:
+        matchObj = re.match( r"^(\d+?)[- _.:;'hH]?(\d{1,2})[mM]?$",  hdebut)
+        if matchObj:
+            hdebut = "{:%H:%M}".format(datetime.strptime(matchObj.group(1)+":"+matchObj.group(2),"%H:%M"))
+        else:
+            return False
+        if not fin_obligatoire and (hfin == False or hfin == ""):
+            return [hdebut,""]
+        matchObj = re.match( r"^(\d+?)[- _.:;'hH]?(\d{1,2})[mM]?$",  hfin)
+        if matchObj:
+            hfin = "{:%H:%M}".format(datetime.strptime(matchObj.group(1)+":"+matchObj.group(2),"%H:%M"))
+        else:
+            return False
+        return [hdebut,hfin]
+    except:
+        return False
+
+def conv_str2minutes(str):
+    (h,m) = str.split(":")
+    return (int(h)*60 + int(m))
+def conv_minutes2str(min):
+    return (str(min/60)+"h "+str(min%60)+"m")
