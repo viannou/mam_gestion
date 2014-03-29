2c88a5f16b81ae071b41a828826641588edb8e29
viannou <viannou@gmail.com>
2014-03-29 01:28:58 +0100

    auto

--- mam_recap.py
+++ mam_recap.py
@@ -14,11 +14,11 @@ class mam_mois_e(osv.Model):
             date_fin = record.avenant_id.date_fin # au format yyyy-mm-dd (ou false s'il n'y en a pas)
             result[record.id] = {}
             result[record.id]['jour_debut'] = 1
-            print date_debut[:7], "{0}-{1}".format(record.annee, record.mois)
-            if date_debut[:7] == "{0}-{1}".format(record.annee, record.mois): # le mois du début du contrat, le jour_début est le premier jour du contrat.
+            print date_debut[:7], "{0}-{1:02d}".format(record.annee, record.mois)
+            if date_debut[:7] == "{0}-{1:02d}".format(record.annee, record.mois): # le mois du début du contrat, le jour_début est le premier jour du contrat.
                 result[record.id]['jour_debut'] = date_debut[8:]
             result[record.id]['jour_fin'] = calendar.monthrange(record.annee, record.mois)[1] # dernier jour du mois
-            if date_fin and date_fin [:7] == "{0}-{1}".format(record.annee, record.mois): # le mois de fin du contrat, le jour_fin est le dernier jour du contrat.
+            if date_fin and date_fin [:7] == "{0}-{1:02d}".format(record.annee, record.mois): # le mois de fin du contrat, le jour_fin est le dernier jour du contrat.
                 result[record.id]['jour_fin'] = date_fin[8:]
         return result
     _columns = {
